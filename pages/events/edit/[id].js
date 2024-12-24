0a6b2c934593265f71fdddba18e0e1f34e1a62c0
Nelson Ngaleumo <nelsonngaleumo@yahoo.fr>
2024-12-23 18:57:43 -0600

    Updated Editform for Events

---
+++ pages/events/edit/[id].js
@@ -0,0 +1,13 @@
+import UpdateEvent from '../../../components/event/UpdateEvent';
+import { useAuth } from '../../../utils/context/authContext';
+
+const EditEvent = () => {
+  const { user } = useAuth();
+  return (
+    <div>
+      <h2>Update Game</h2>
+      <UpdateEvent user={user} />
+    </div>
+  );
+};
+export default EditEvent;
